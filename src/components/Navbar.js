6a69c7bed5062d919f158bcd449057a4d7733437
Raghav Joshi <raghavprojects7696@gmail.com>
2024-09-07 13:30:40 +0530

    React route dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
     const {navTitle = 'dfdf', mode, toggleMode} = props;
     return (
         <nav className={`navbar navbar-expand-lg bg-${mode}`} data-bs-theme={mode}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/public">{navTitle}</a>
+                <Link className="navbar-brand" to="/">{navTitle}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -15,10 +16,10 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">About</a>
+                            <Link className="nav-link active" aria-current="page" to="/about">About</Link>
                         </li>
                     </ul>
                 </div>
